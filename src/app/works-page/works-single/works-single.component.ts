957885a355ebfdceedf5ea59c9d15f8e656a9a8e
Alessandro Bencini <alessandrobencini@MBPdiAlessandro.Home>
2022-03-03 15:16:15 +0100

    added work embedded apple music

--- src/app/works-page/works-single/works-single.component.ts
+++ src/app/works-page/works-single/works-single.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { WorksService } from 'src/app/works.service';
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl,
+} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-works-single',
   templateUrl: './works-single.component.html',
-  styleUrls: ['./works-single.component.scss']
+  styleUrls: ['./works-single.component.scss'],
 })
 export class WorksSingleComponent implements OnInit {
   work;
@@ -14,22 +19,25 @@ export class WorksSingleComponent implements OnInit {
 
   constructor(
     private worksService: WorksService,
-    private route: ActivatedRoute
-    ) { }
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  sanitizeUrl(work) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://' + work + '/'
+    );
+  }
 
   ngOnInit() {
     this.lang = localStorage.getItem('lang') || 'en';
 
-    this.route.params.subscribe(params => {
-       this.id = params['id'];
-    })
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    });
 
-      this.worksService
-        .getSingleWork(this.id)
-        .subscribe(work => {
-          this.work = work;
-      })
-    
+    this.worksService.getSingleWork(this.id).subscribe((work) => {
+      this.work = work;
+    });
   }
-
 }

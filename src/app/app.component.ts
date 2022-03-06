d47e2ef37abf37020c7d536d6851a30ff1415b2c
Alessandro <als.bencini@gmail.com>
2022-03-06 23:05:09 +0100

    new branch for multilangual support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Alessandro Bencini';
 
-  constructor(private translateService: TranslateService){
-    this.translateService.setDefaultLang('en');
-    this.translateService.use(localStorage.getItem('lang') || 'en');
+  constructor(private translateService: TranslateService) {
+    this.translateService.addLangs(['en', 'ita']);
+    this.translateService.setDefaultLang('ita');
+    this.translateService.use(localStorage.getItem('lang') || 'ita');
   }
 }
